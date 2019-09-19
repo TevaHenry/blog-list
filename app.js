66c619b84e3553417a9bf1345e597eacd3ec1a1d
Teva Henry <35941084+TevaHenry@users.noreply.github.com>
2019-09-18 14:19:57 -1000

    Initial commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const mongoose = require('mongoose')
+
+const config = require('./utils/config')
+const blogsRouter = require('./controllers/blogs')
+
+mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true })
+
+app.use(cors())
+app.use(bodyParser.json())
+
+app.use('/api/blogs', blogsRouter)
+
+module.exports = app
